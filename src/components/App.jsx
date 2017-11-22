8df02882beb80eab767c866d8c553030e54e794a
lamemmv <lamemmv@gmail.com>
2017-11-22 23:16:05 +0700

    update config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Button, Container, Header } from 'semantic-ui-react';
 
 export default class App extends Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World 1s</h1>
-      </div>);
+      <div style={{ textAlign: 'center' }}>
+        <Container>
+          <Header as='h1'>Hello world!</Header>
+
+          <Button
+            content='Discover docs'
+            href='http://react.semantic-ui.com'
+            icon='github'
+            labelPosition='left'
+          />
+        </Container>
+      </div>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,84 @@
 /*
     ./webpack.config.js
 */
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+const analyze = !!process.env.ANALYZE_ENV;
+const env = process.env.NODE_ENV || 'development';
+
+const webpackConfig = {
+    name: 'client',
+    target: 'web',
+
+    entry: {
+        app: path.resolve('src/index.js'),
+    },
+
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(env),
+            },
+        }),
+        HtmlWebpackPluginConfig,
+    ],
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve('src/dist'),
+        publicPath: '/',
+    },
+
+    resolve: {
+        modules: [
+            path.resolve('src'),
+            'node_modules',
+        ],
+        extensions: ['.js', '.jsx'],
+    },
+}
+
+if (analyze) {
+    webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+}
+
+if (env === 'production') {
+    webpackConfig.plugins.push(
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false,
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                unused: true,
+                dead_code: true,
+                warnings: false,
+            },
+        })
+    )
+}
+
+module.exports = webpackConfig;
+
+/*const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
@@ -23,4 +100,4 @@ module.exports = {
         ]
     },
     plugins: [HtmlWebpackPluginConfig]
-}
+}*/
